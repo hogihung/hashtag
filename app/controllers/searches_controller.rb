3e84ac01bd78e78a9119c0660735ae5ad0c4cf0a
hogihung <hogihung@gmail.com>
2014-07-28 17:12:36 -0400

    Search Twitter live for a hashtag value.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,14 @@ class SearchesController < ApplicationController
   end
 
   def show
-    #@tweets = Twitter.search("##{params[:id]}").results
-    @tweets = Twitter.search("Rails").results
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+
+    @tweets = client.search("##{params[:id]}").to_a
   end
 
 end
--- spec/integration/search_twitter_spec.rb
+++ spec/integration/search_twitter_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 feature 'Search twitter for a term' do 
   scenario 'searching by a hast tag' do 
     visit root_path
-    fill_in 'Search', with: 'Rails'
+    fill_in 'Search', with: 'TechTalk'
     click_button 'Search'
-    expect(page).to have_css 'li.tweet', count: 15
+    expect(page).to have_css 'li.tweet', count: 100
+
+    all('li.tweet').each do |tweet|
+      expect(tweet.text).to match /#TechTalk/i 
+    end
   end
 end
